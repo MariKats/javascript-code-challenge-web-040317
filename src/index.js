836c52a7a7239b27fefe34f048ad17be663599f7
github username <github email address>
2017-05-26 09:57:54 -0400

    added form not blank validation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
   $("#note-form").on('submit', function(e){
     e.preventDefault()
-
     const text = $('#text').val()
     const list = new CommentList()
     list.addComment(text)
+
+    if (text === "") {
+      alert("You can't submit an empty form. Please enter your comment.")
+    } else {
     $('#comment-list').append(list.render())
+    }
   })
 })
